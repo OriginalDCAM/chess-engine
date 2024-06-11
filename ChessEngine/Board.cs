f25fca65cacdf1b054c33a498bce67e74bc0bbb3
Denzel // TrueStatement <denzelannette@hotmail.com>
2024-06-11 23:02:01 +0200

    - Moved some of the methods into their own respective class

--- ChessEngine/Board.cs
+++ ChessEngine/Board.cs
@@ -1,79 +1,63 @@
-﻿namespace ChessEngine;
+﻿using System.Diagnostics;
+
+namespace ChessEngine;
 
 public class Board
 {
-    public enum Piece
-    {
-        WhitePawn,
-        WhiteKnight,
-        WhiteBishop,
-        WhiteRook,
-        WhiteQueen,
-        WhiteKing,
+    public HashSet<string> FenList { get; } = new();
 
-        BlackPawn,
-        BlackKnight,
-        BlackBishop,
-        BlackRook,
-        BlackQueen,
-        BlackKing
-    }
+    public Player CanMove { get; set; } = Player.White;
 
-    public HashSet<string> FenList { get; private set; } = new();
+    public string LastAddedFen { get; set; }
 
     public ulong[] Bitboard { get; } = new ulong[12];
 
+
     public Board()
     {
-        GenerateBoardWithFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq c6 0 2");
+        GenerateBoardWithFen();
         GenerateBoardWithFen("8/5k2/3p4/1p1Pp2p/pP2Pp1P/P4P1K/8/8 b - - 99 50");
-        PrintChessboard();
     }
 
-                public bool GenerateBoardWithFen(string fen)
-    
+    public void GenerateBoardWithFen(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq c6 0 2")
     {
-        Console.WriteLine("Generating board with FEN: " + fen);
         if (Bitboard.Any(bitboard => bitboard != 0)) Array.Clear(Bitboard);
 
         string[] fenParts = fen.Split(' ');
         string[] ranks = fenParts[0].Split('/');
 
-        try
-        {
-            if (ranks.Length != 8) throw new ArgumentException("Invalid FEN: " + fen);
 
-            for (var rank = 0; rank < 8; rank++)
+        if (ranks.Length > 8)
+        {
+            throw new FormatException("Invalid FEN: " + fen);
+        }
+
+        for (var rank = 0; rank < 8; rank++)
+        {
+            var file = 0;
+            foreach (var character in ranks[rank])
             {
-                var file = 0;
-                foreach (var character in ranks[rank])
+                if (char.IsDigit(character))
                 {
-                    if (char.IsDigit(character))
-                    {
-                        file += int.Parse(character.ToString());
-                        continue;
-                    }
-
-                    var pieceIndex = GetPieceIndex(character);
-                    var squareIndex = rank * 8 + file;
-                    var pieceBit = 1UL << squareIndex;
-                    Bitboard[pieceIndex] |= pieceBit;
-                    file++;
+                    file += int.Parse(character.ToString());
+                    continue;
                 }
-            }
 
-            FenList.Add(fen);
-            return true;
+                var pieceIndex = Piece.GetPieceIndex(character);
+                var squareIndex = rank * 8 + file;
+                var pieceBit = 1UL << squareIndex;
+                Bitboard[pieceIndex] |= pieceBit;
+                file++;
+            }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-            return false;
-        }
+
+        FenList.Add(fen);
+        LastAddedFen = fen;
     }
 
-    public bool Move(int fromSquare, int toSquare, ulong fromBitboard)
+    public bool Move(int fromSquare, int toSquare, ulong fromBitboard, Player colour)
     {
+        if (colour != CanMove) return false;
         var fromMask = 1UL << fromSquare;
         var toMask = 1UL << toSquare;
 
@@ -82,47 +66,17 @@ public class Board
         var pieceIndex = Array.IndexOf(Bitboard, fromBitboard);
         Bitboard[pieceIndex] &= ~fromMask;
         Bitboard[pieceIndex] |= toMask;
-    return false;
+        CanMove = (Player.White == CanMove) ? Player.Black : Player.White;
+        Console.WriteLine($"this player can now move: {CanMove}");
+        return true;
     }
 
-    private int GetPieceIndex(char piece)
+    public List<Move> GenerateMoves()
     {
-        switch (piece)
-        {
-            case 'p': return (int) Piece.BlackPawn;
-            case 'n': return (int) Piece.BlackKnight;
-            case 'b': return (int) Piece.BlackBishop;
-            case 'r': return (int) Piece.BlackRook;
-            case 'q': return (int) Piece.BlackQueen;
-            case 'k': return (int) Piece.BlackKing;
-            case 'P': return (int) Piece.WhitePawn;
-            case 'N': return (int) Piece.WhiteKnight;
-            case 'B': return (int) Piece.WhiteBishop;
-            case 'R': return (int) Piece.WhiteRook;
-            case 'Q': return (int) Piece.WhiteQueen;
-            case 'K': return (int) Piece.WhiteKing;
-            default: return -1;
-        }
-    }
+        var moves = new List<Move>();
 
-    private string GetPieceSymbol(int pieceIndex)
-    {
-        switch (pieceIndex)
-        {
-            case (int) Piece.WhitePawn: return "p";
-            case (int) Piece.WhiteKnight: return "n";
-            case (int) Piece.WhiteBishop: return "b";
-            case (int) Piece.WhiteRook: return "r";
-            case (int) Piece.WhiteQueen: return "q";
-            case (int) Piece.WhiteKing: return "k";
-            case (int) Piece.BlackPawn: return "P";
-            case (int) Piece.BlackKnight: return "N";
-            case (int) Piece.BlackBishop: return "B";
-            case (int) Piece.BlackRook: return "R";
-            case (int) Piece.BlackQueen: return "Q";
-            case (int) Piece.BlackKing: return "K";
-            default: return " ";
-        }
+
+        return moves;
     }
 
     public char GetPieceSymbolAtSquare(int squareIndex)
@@ -131,24 +85,16 @@ public class Board
         {
             var bitboard = Bitboard[pieceIndex];
             var mask = 1UL << squareIndex;
-            if ((bitboard & mask) != 0) return GetPieceSymbol(pieceIndex)[0];
+            if ((bitboard & mask) != 0) return Piece.GetPieceSymbol(pieceIndex);
         }
 
         return '.';
     }
 
-    public void PrintChessboard()
+    public Player GetColorAtSquare(int squareIndex)
     {
-        for (var rank = 0; rank < 8; rank++)
-        {
-            for (var fileIndex = 0; fileIndex < 8; fileIndex++)
-            {
-                var squareIndex = rank * 8 + fileIndex;
-                var pieceSymbol = GetPieceSymbolAtSquare(squareIndex);
-                Console.Write(pieceSymbol + " ");
-            }
-
-            Console.WriteLine();
-        }
+        char pieceSymbol = GetPieceSymbolAtSquare(squareIndex);
+        
+        return char.IsUpper(pieceSymbol) ? Player.Black : Player.White;
     }
 }
