d3e1dc964781cb77e3afd8706285728a05661def
Denzel // TrueStatement <denzelannette@hotmail.com>
2024-06-11 22:59:36 +0200

    - Seperated the logic and ui to their own files

---
+++ ChessWebUI/Components/Addons/FenList.razor.cs
@@ -0,0 +1,22 @@
+﻿using ChessEngine;
+using Microsoft.AspNetCore.Components;
+using ChessWebUI.Components;
+
+namespace ChessWebUI.Components.Addons;
+
+public partial class FenList : ComponentBase
+{
+    [Parameter] public required Action<string> AddFen { get; set; }
+    
+    [Parameter] public required string LastAddedFen { get; set; }
+    
+    [Parameter] public required HashSet<string> ListOfFens { get; set; }
+
+
+    public required string InputFen { get; set; }
+
+    private void OnInput(string value)
+    {
+        InputFen = value;
+    }
+}
---
+++ ChessWebUI/Components/Chessboard.razor.cs
@@ -0,0 +1,51 @@
+﻿using ChessEngine;
+using Microsoft.AspNetCore.Components;
+
+namespace ChessWebUI.Components;
+
+public partial class Chessboard
+{
+    [Parameter] public required Board Board { get; set; }
+    
+    private int _selectedSquare = -1; // Track selected square
+
+    private void OnSquareClick(int square)
+    {
+        if (_selectedSquare == -1) // No piece selected, select this one
+        {
+            if (Board.GetPieceSymbolAtSquare(square) != '.')
+            {
+                _selectedSquare = square;
+            }
+        }
+        else // Piece already selected, move it
+        {
+            Move(_selectedSquare, square, Board.GetColorAtSquare(_selectedSquare));
+            _selectedSquare = -1; // Deselect after move
+        }
+    }
+
+    private void Move(int from, int to, Player colour)
+    {
+        foreach (var bitboard in Board.Bitboard)
+        {
+            var hasMoved = Board.Move(from, to, bitboard, colour);
+            if (hasMoved) break;
+        }
+    }
+
+    private void AddFenToBoard(string? fen)
+    {
+        if (fen == null) return;
+        try
+        {
+            Board.GenerateBoardWithFen(fen);
+            StateHasChanged();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+}
