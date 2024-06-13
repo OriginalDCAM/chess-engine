e146c28a531523487bb911bda3e87f7f212f4f93
Denzel // TrueStatement <denzelannette@hotmail.com>
2024-06-13 15:53:10 +0200

    Updated so fen pieces work the intended way.

--- ChessEngine/Piece.cs
+++ ChessEngine/Piece.cs
@@ -4,37 +4,37 @@ public static class Piece
 {
     public enum PieceTypes
     {
-        WhitePawn = 0,
-        WhiteKnight = 1,
-        WhiteBishop = 2,
-        WhiteRook = 3,
-        WhiteQueen = 4,
-        WhiteKing = 5,
+        WhitePawn,
+        WhiteKnight,
+        WhiteBishop,
+        WhiteRook,
+        WhiteQueen,
+        WhiteKing,
 
-        BlackPawn = 6,
-        BlackKnight = 7,
-        BlackBishop = 8,
-        BlackRook = 9,
-        BlackQueen = 10,
-        BlackKing = 11
+        BlackPawn,
+        BlackKnight,
+        BlackBishop,
+        BlackRook,
+        BlackQueen,
+        BlackKing
     }
 
     public static int GetPieceIndex(char piece)
     {
         switch (piece)
         {
-            case 'P': return (int) PieceTypes.BlackPawn;
-            case 'N': return (int) PieceTypes.BlackKnight;
-            case 'B': return (int) PieceTypes.BlackBishop;
-            case 'R': return (int) PieceTypes.BlackRook;
-            case 'Q': return (int) PieceTypes.BlackQueen;
-            case 'K': return (int) PieceTypes.BlackKing;
-            case 'p': return (int) PieceTypes.WhitePawn;
-            case 'n': return (int) PieceTypes.WhiteKnight;
-            case 'b': return (int) PieceTypes.WhiteBishop;
-            case 'r': return (int) PieceTypes.WhiteRook;
-            case 'q': return (int) PieceTypes.WhiteQueen;
-            case 'k': return (int) PieceTypes.WhiteKing;
+            case 'p': return (int) PieceTypes.BlackPawn;
+            case 'n': return (int) PieceTypes.BlackKnight;
+            case 'b': return (int) PieceTypes.BlackBishop;
+            case 'r': return (int) PieceTypes.BlackRook;
+            case 'q': return (int) PieceTypes.BlackQueen;
+            case 'k': return (int) PieceTypes.BlackKing;
+            case 'P': return (int) PieceTypes.WhitePawn;
+            case 'N': return (int) PieceTypes.WhiteKnight;
+            case 'B': return (int) PieceTypes.WhiteBishop;
+            case 'R': return (int) PieceTypes.WhiteRook;
+            case 'Q': return (int) PieceTypes.WhiteQueen;
+            case 'K': return (int) PieceTypes.WhiteKing;
             default: return -1;
         }
     }
@@ -43,18 +43,18 @@ public static class Piece
     {
         switch (pieceIndex)
         {
-            case (int) PieceTypes.WhitePawn: return 'p';
-            case (int) PieceTypes.WhiteKnight: return 'n';
-            case (int) PieceTypes.WhiteBishop: return 'b';
-            case (int) PieceTypes.WhiteRook: return 'r';
-            case (int) PieceTypes.WhiteQueen: return 'q';
-            case (int) PieceTypes.WhiteKing: return 'k';
-            case (int) PieceTypes.BlackPawn: return 'P';
-            case (int) PieceTypes.BlackKnight: return 'N';
-            case (int) PieceTypes.BlackBishop: return 'B';
-            case (int) PieceTypes.BlackRook: return 'R';
-            case (int) PieceTypes.BlackQueen: return 'Q';
-            case (int) PieceTypes.BlackKing: return 'K';
+            case (int) PieceTypes.WhitePawn: return 'P';
+            case (int) PieceTypes.WhiteKnight: return 'N';
+            case (int) PieceTypes.WhiteBishop: return 'B';
+            case (int) PieceTypes.WhiteRook: return 'R';
+            case (int) PieceTypes.WhiteQueen: return 'Q';
+            case (int) PieceTypes.WhiteKing: return 'K';
+            case (int) PieceTypes.BlackPawn: return 'p';
+            case (int) PieceTypes.BlackKnight: return 'n';
+            case (int) PieceTypes.BlackBishop: return 'b';
+            case (int) PieceTypes.BlackRook: return 'r';
+            case (int) PieceTypes.BlackQueen: return 'q';
+            case (int) PieceTypes.BlackKing: return 'k';
             default: return ' ';
         }
     }
