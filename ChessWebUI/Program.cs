0bbbfb75dea31f65bb20a2640e4ca8e5a161e204
TrueStatement <denzelannette@hotmail.com>
2024-08-03 19:54:01 +0200

    + Added mudblazor

--- ChessWebUI/Program.cs
+++ ChessWebUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 namespace ChessWebUI;
 
@@ -10,6 +11,8 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+        
+        builder.Services.AddMudServices();
 
         builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
         await builder.Build().RunAsync();
