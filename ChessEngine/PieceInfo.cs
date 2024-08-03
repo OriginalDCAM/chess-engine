98fd7a20f7a027abc5caa96bc9bf12727917f6fd
TrueStatement <denzelannette@hotmail.com>
2024-08-03 19:55:32 +0200

    + Added an struct for PieceInfo with the Square index the piece is on and what type of piece it is.

---
+++ ChessEngine/PieceInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ChessEngine;
+
+public struct PieceInfo
+{
+    public readonly int SquareIndex;
+    public readonly int PieceIndex;
+
+    public PieceInfo(int squareIndex, int pieceIndex)
+    {
+        SquareIndex = squareIndex;
+        PieceIndex = pieceIndex;
+    }
+
+}
