0a5992a19ec5aba76d71b29f23e021aad1aa09f9
TrueStatement <denzelannette@hotmail.com>
2024-09-18 22:25:33 +0200

    + Struct that maps the castling rights so i can easily store them after reading the fen to possibly allow castling.

---
+++ ChessEngine/CastlingRights.cs
@@ -0,0 +1,23 @@
+﻿namespace ChessEngine;
+
+public struct CastlingRights
+{
+    public bool WhiteKingSide { get; set; }
+    public bool WhiteQueenSide { get; set; }
+    public bool BlackKingSide { get; set; }
+    public bool BlackQueenSide { get; set; }
+    
+    public CastlingRights(bool whiteKingSide, bool whiteQueenSide, bool blackKingSide, bool blackQueenSide)
+    {
+        WhiteKingSide = whiteKingSide;
+        WhiteQueenSide = whiteQueenSide;
+        BlackKingSide = blackKingSide;
+        BlackQueenSide = blackQueenSide;
+    }
+    
+    public override string ToString()
+    {
+        return
+            $"{(WhiteKingSide ? "K" : "")}{(WhiteQueenSide ? "Q" : "")}{(BlackKingSide ? "k" : "")}{(BlackQueenSide ? "q" : "")}";
+    }
+}
