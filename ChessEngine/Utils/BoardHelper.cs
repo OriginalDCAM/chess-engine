2bb2c5014e478168d1d4adaef58fed8b5b6eee5e
TrueStatement <denzelannette@hotmail.com>
2024-09-18 22:19:37 +0200

    + Added helper functions for Easily getting the rank and file position

---
+++ ChessEngine/Utils/BoardHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessEngine.Utils;
+
+public static class BoardHelper
+{
+    public static int GetRankPosition(int square) => square / 8;
+
+    public static int GetFilePosition(int square) => square % 8;
+
+    public static string ConvertSquareToSan(int square)
+    {
+        // Reverse
+        string[] file = { "a", "b", "c", "d", "e", "f", "g", "h" };
+        
+        return $"{file[GetFilePosition(square)]}{8 - GetRankPosition(square)}";
+    }
+    
+    public static string ConvertMoveToSan(Move move)
+    {
+        return $"{ConvertSquareToSan(move.TargetSquare)}";
+    }
+    
+}
