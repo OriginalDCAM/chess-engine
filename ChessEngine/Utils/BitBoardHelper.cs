3b5f21750e1fe7a078648c5232935aee42aeb34f
OriginalDCAM <denzelannette@hotmail.com>
2024-09-23 11:02:08 +0200

    Added a bitboard helper class to make bit operations easier.

---
+++ ChessEngine/Utils/BitBoardHelper.cs
@@ -0,0 +1,14 @@
+namespace ChessEngine.Utils;
+
+public static class BitBoardHelper 
+{
+    public static void SetSquare(ref ulong bitboard, int squareIndex) {}
+    
+    public static void ClearSquare(ref ulong bitboard, int squareIndex) {}
+    
+    public static void ToggleSquare(ref ulong bitboard, int squareIndex) {}
+    
+    public static void ToggleSquares(ref ulong bitboard, int squareA, int squareB) {}
+    
+    public static void ContainsSquare(ulong bitboard, int squareIndex) {}
+}
